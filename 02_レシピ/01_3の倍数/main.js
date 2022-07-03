0458e517600efc1c2a97a7459600a327c9d92507
yuka-yuttie <onigirisuguha@gmail.com>
2022-07-03 19:22:34 +0900

    [FizzBuzz]を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -2,8 +2,12 @@
 const genkiFunction = function(number) {
     // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
     for (let n = 1; n <= number; n++) {
-      if (n % 3 === 0) {
-        console.log(`${n}!!!!!!!`)
+      if (n % 3 === 0  && n % 5 === 0) {
+        console.log(`FizzBuzz`) 
+      } else if (n % 5 === 0) {
+        console.log(`Buzz`)
+      } else if (n % 3 === 0) {
+        console.log(`Fizz`)
       } else {
         console.log(n)
       }
