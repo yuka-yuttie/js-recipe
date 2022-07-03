a46f841eedbc3de49e822c866e6563c332277a05
yuka-yuttie <onigirisuguha@gmail.com>
2022-07-03 21:35:10 +0900

    [便利なボタン]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -25,3 +25,20 @@ multiplyButton.onclick = function() {
     // count を表示
     display.textContent = count
 }
+
+function get_calc(btn) {
+    if(btn.value == "=") {
+      document.dentaku.display.value = eval(document.dentaku.display.value);
+    } else if (btn.value == "C") {
+      document.dentaku.display.value = "";
+    } else {
+      if (btn.value == "×") {
+        btn.value = "*";
+      } else if (btn.value == "÷") {
+        btn.value = "/";
+      } 
+      document.dentaku.display.value += btn.value;
+      document.dentaku.multi_btn.value = "×";
+      document.dentaku.div_btn.value = "÷";
+    }
+  }
