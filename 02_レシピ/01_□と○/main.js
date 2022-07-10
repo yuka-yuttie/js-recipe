63f5657a4d2700baf301aff8855e0fd6136f154c
yuka-yuttie <onigirisuguha@gmail.com>
2022-07-10 20:46:01 +0900

    ゼミでクイズを作りました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,10 @@
 const figure = document.getElementById("figure")
+const square = document.getElementById("square")
 
 figure.onclick = function() {
   figure.classList.toggle("rounded")
 }
+
+square.onclick = function() {
+    square.classList.toggle("rounded")
+  }
